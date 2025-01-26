22664b4ac4e7c38677c06e4c441051a410057d0f
Hugo Lombana <favyan16@gmail.com>
2025-01-26 16:50:50 +0000

    instrument Honeycomb

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,7 +14,29 @@ from services.messages import *
 from services.create_message import *
 from services.show_activity import *
 
+#HoneyComb .........
+from opentelemetry import trace 
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.instrumentation.request import RequestInstrumentor
+from opentelemetry.exporter.otpl.proto.http.trace_exporter import OTPLSpanExporter
+from opentelemetry.sdk.trace import TraceProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+#HoneyComb .........
+#Initialize tracing and an exporter that can send data to HoneyComb
+provider = TraceProvider()
+processor = BatchSpanProcessor(OTLPSpanExporter())
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+tracer = trace.get_tracer(_name_)
+
 app = Flask(__name__)
+
+#HoneyComb ....abs
+#Initialize automatic instrumentation with flask
+FlaskInstrumentor().instrument_app(app)
+RequestInstrumentor().instrument()
+
 frontend = os.getenv('FRONTEND_URL')
 backend = os.getenv('BACKEND_URL')
 origins = [frontend, backend]
