207b76cd88f5148d3145f4489c5a32d52eb12cf6
Hugo Lombana <favyan16@gmail.com>
2025-03-06 03:54:37 +0000

    final setup cognito

--- frontend-react-js/src/aws-exports.js
+++ frontend-react-js/src/aws-exports.js
@@ -1,14 +1,21 @@
 const awsExports = {
   Auth: {
-    region: process.env.REACT_APP_AWS_COGNITO_REGION,
-    userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOLS_ID,
-    userPoolWebClientId: process.env.REACT_APP_CLIENT_ID,
-    mandatorySignIn: true,
-    authenticationFlowType: "USER_SRP_AUTH",
-  },
+      Cognito: {
+          region: process.env.REACT_APP_AWS_COGNITO_REGION,
+          userPoolId: process.env.REACT_APP_AWS_COGNITO_USER_POOLS_ID,
+          userPoolClientId: process.env.REACT_APP_CLIENT_ID,
+      },
+      oauth: {
+          domain: process.env.REACT_APP_OAUTH_DOMAIN,
+          scope: ['email', 'openid', 'profile'],
+          redirectSignIn: process.env.REACT_APP_REDIRECT_SIGN_IN,
+          redirectSignOut: process.env.REACT_APP_REDIRECT_SIGN_OUT,
+          responseType: 'code',
+      }
+  }
 };
-
 export default awsExports;
+
 
 
 
